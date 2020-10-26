4725575982ee9091efe49ce54533286d4d12d31d
KexinWen <kencywen@gmail.com>
2020-10-26 15:17:45 -0700

    Add loading more data and scroll to top function

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Topic from './components/Topic';
 import List from './components/List';
 import Recommend from './components/Recommend';
 import Writer from './components/Writer';
+import { actionCreators } from './store';
+import { BackTop } from './style';
+
 
 import { 
     HomeWrapper,
@@ -11,26 +15,62 @@ import {
 } from './style';
 
 class Home extends Component {
+    handleScrollTop() {
+        window.scrollTo(0, 0);
+    }
+
     render() {
         return (
-                <HomeWrapper>
-                    <HomeLeft>
-                        <img 
-                            className='banner-img' 
-                            alt=''
-                            src="https://upload-images.jianshu.io/upload_images/1897565-33a6f469da706c0e?imageMogr2/auto-orient/strip|imageView2/2/w/640/format/webp" 
-                        />
-                        <Topic />
-                        <List />
-                    </HomeLeft>
-                    <HomeRight>
-                        <Recommend />
-                        <Writer />
-                    </HomeRight>
-                </HomeWrapper>
+            <HomeWrapper>
+                <HomeLeft>
+                    <img 
+                        className='banner-img' 
+                        alt=''
+                        src="https://upload.jianshu.io/users/upload_avatars/9988193/fc26c109-1ae6-4327-a298-2def343e9cd8.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp" 
+                    />
+                    <Topic />
+                    <List />
+                </HomeLeft>
+                <HomeRight>
+                    <Recommend />
+                    <Writer />
+                </HomeRight>
+                { this.props.showScroll ? <BackTop onClick={this.handleScrollTop}>顶部</BackTop> : null}
+                
+            </HomeWrapper>
         )
     }
-}
 
-export default Home;
+    componentDidMount() {
+        this.props.changeHomeData();
+        this.bindEvents();
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.props.changeScrollTopShow);
+    }
+
+    bindEvents() {
+        window.addEventListener('scroll', this.props.changeScrollTopShow);
+    }
+}
+const mapState = (state) => ({
+    showScroll: state.getIn(['home', 'showScroll'])
+})
+
+const mapDispatch = (dispatch) => ({
+    changeHomeData() {
+        const action = actionCreators.getHomeInfo();
+        dispatch(action);
+    }, 
+    changeScrollTopShow() {
+        if (document.documentElement.scrollTop > 100) {
+            dispatch(actionCreators.toggleTopShow(true));
+        } else {
+            dispatch(actionCreators.toggleTopShow(false));
+        }
+    }
+});
+
+export default connect(mapState, mapDispatch)(Home);
 
--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -1,49 +1,30 @@
 
 import { fromJS } from 'immutable';
+import * as constants from './constants';
 
 const defaultState = fromJS({
-    topicList: [{
-        id: 1,
-        title: '社会热点',
-        imgUrl: 'https://upload.jianshu.io/users/upload_avatars/9988193/fc26c109-1ae6-4327-a298-2def343e9cd8.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp'
-    }, {
-        id: 2,
-        title: '手绘',
-        imgUrl: 'https://upload.jianshu.io/users/upload_avatars/9988193/fc26c109-1ae6-4327-a298-2def343e9cd8.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp'
-    }],
-    articleList: [{
-        id: 1,
-        title: '缘起·黄巾（四十）',
-        desc: '夜半无眠思过往，今生已过万余天。莫再闲暇多心事，愿君归来是少年。 （一）张顺做梦 当张顺自以为高枕无忧时，张宝已经去那事先约定的张家山上等张角了',
-        imgUrl: 'https://upload-images.jianshu.io/upload_images/16726825-7ca97c84c9f12102.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
-    }, {
-        id: 2,
-        title: '缘起·黄巾（四十）',
-        desc: '夜半无眠思过往，今生已过万余天。莫再闲暇多心事，愿君归来是少年。 （一）张顺做梦 当张顺自以为高枕无忧时，张宝已经去那事先约定的张家山上等张角了',
-        imgUrl: 'https://upload-images.jianshu.io/upload_images/16726825-7ca97c84c9f12102.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
-    }, {
-        id: 3,
-        title: '缘起·黄巾（四十）',
-        desc: '夜半无眠思过往，今生已过万余天。莫再闲暇多心事，愿君归来是少年。 （一）张顺做梦 当张顺自以为高枕无忧时，张宝已经去那事先约定的张家山上等张角了',
-        imgUrl: 'https://upload-images.jianshu.io/upload_images/16726825-7ca97c84c9f12102.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
-    }, {
-        id: 4,
-        title: '缘起·黄巾（四十）',
-        desc: '夜半无眠思过往，今生已过万余天。莫再闲暇多心事，愿君归来是少年。 （一）张顺做梦 当张顺自以为高枕无忧时，张宝已经去那事先约定的张家山上等张角了',
-        imgUrl: 'https://upload-images.jianshu.io/upload_images/16726825-7ca97c84c9f12102.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
-    }],
-    recommendList: [{
-        id: 1,
-        imgUrl:"https://cdn2.jianshu.io/assets/web/banner-s-club-aa8bdf19f8cf729a759da42e4a96f366.png"
-    }, {
-        id: 2,
-        imgUrl:"https://cdn2.jianshu.io/assets/web/banner-s-7-1a0222c91694a1f38e610be4bf9669be.png"
-    }]
+    topicList: [],
+    articleList: [],
+    recommendList: [],
+    articlePage: 1,
+    showScroll: false
 });
 
 export default(state = defaultState, action) => {
     switch(action.type) {
-        
+        case constants.CHANGE_HOME_DATA:
+            return state.merge({
+                topicList: fromJS(action.topicList),
+                articleList: fromJS(action.articleList),
+                recommendList: fromJS(action.recommendList)
+            });
+        case constants.ADD_ARTICLE_LIST:
+            return state.merge({
+                'articleList': state.get('articleList').concat(action.list),
+                'articlePage': action.nextPage
+            });
+        case constants.TOGGLE_SCROLL_TOP:
+            return state.set('showScroll', action.show);
         default:
             return state;
     }
