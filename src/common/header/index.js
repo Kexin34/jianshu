f817cec72b76725261b9acc5330110c615939685
KexinWen <kencywen@gmail.com>
2020-10-26 18:16:11 -0700

    Use redux to manage data in detail page

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { CSSTransition } from 'react-transition-group';
 import  { actionCreators } from './store';
 
@@ -63,7 +64,9 @@ class Header extends Component {
         const { focused, handleInputFocus,handleInputBlur, list } = this.props;
         return (
             <HeaderWrapper>
-                <Logo />
+                <Link to='/'>
+                    <Logo />
+                </Link>
                 <Nav>
                     <NavItem className='left active'>首页</NavItem>
                     <NavItem className='left'>下载App</NavItem>
